e8df9d2206116c174cbf0eaf2429a4978e528fa2
Allenyou <i@allenyou.wang>
2025-12-21 00:26:33 +0800

    修复缩进

--- configuration.nix
+++ configuration.nix
@@ -1,62 +1,52 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
+	imports = [
+		./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "nixos-server";
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	networking.hostName = "nixos-server";
 
-  time.timeZone = "Asia/Shanghai";
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+	time.timeZone = "Asia/Shanghai";
+	nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  i18n.defaultLocale = "en_US.UTF-8";
+	i18n.defaultLocale = "en_US.UTF-8";
 
-  users.users.allenyou = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
-    openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFhiR8t5soqwfpcemRLTe9StCSGsD36TyJgE9PGcjJk cfan7@Allenyou-Laptop"
-    ];
-  };
+	users.users.allenyou = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+		packages = with pkgs; [
+			tree
+		];
+		openssh.authorizedKeys.keys = [
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFhiR8t5soqwfpcemRLTe9StCSGsD36TyJgE9PGcjJk cfan7@Allenyou-Laptop"
+		];
+	};
 
-  environment.systemPackages = with pkgs; [
-    vim 
-    wget
-    git
-  ];
-  environment.variables.EDITOR = "vim";
-
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+	environment.systemPackages = with pkgs; [
+		vim
+		wget
+		git
+	];
+	environment.variables.EDITOR = "vim";
 
 
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no"; # disable root login
-      PasswordAuthentication = false; # disable password login
-    };
-    ports = [ 2333 ];
-    openFirewall = true;
-  };
+	services.openssh = {
+		enable = true;
+		settings = {
+			PermitRootLogin = "no";
+			PasswordAuthentication = false;
+		};
+		ports = [ 2333 ];
+		openFirewall = true;
+	};
 
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-
-  nix.settings.substituters = [
-    "https://mirrors.ustc.edu.cn/nix-channels/store"
-  ];
-  system.stateVersion = "25.05"; # Did you read the comment?
+	nix.settings.substituters = [
+		"https://mirrors.ustc.edu.cn/nix-channels/store"
+	];
+	system.stateVersion = "25.05";
 
 }
 
--- flake.nix
+++ flake.nix
@@ -1,25 +1,25 @@
 {
-  description = "Allenyou's NixOS Server Flake";
+	description = "Allenyou's NixOS Server Flake";
 
-  inputs = {
-    nixpkgs.url = "git+https://mirrors.nju.edu.cn/git/nixpkgs.git?ref=nixos-25.05&shallow=1";
-    home-manager = {
-      url = "git+https://gitee.com/Allenyou1126/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+	inputs = {
+		nixpkgs.url = "git+https://mirrors.nju.edu.cn/git/nixpkgs.git?ref=nixos-25.05&shallow=1";
+		home-manager = {
+			url = "git+https://gitee.com/Allenyou1126/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.nixos-server = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.allenyou = import ./home.nix;
-	  home-manager.extraSpecialArgs = inputs;
-        }
-      ];
-    };
-  };
+	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+		nixosConfigurations.nixos-server = nixpkgs.lib.nixosSystem {
+			modules = [
+				./configuration.nix
+				home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.allenyou = import ./home.nix;
+                    home-manager.extraSpecialArgs = inputs;
+				}
+			];
+		};
+	};
 }
