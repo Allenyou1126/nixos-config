60286570b5ffa013abebc5f6c11c2ae63f2e7d44
Allenyou <i@allenyou.wang>
2025-12-23 17:05:36 +0800

    修复

--- user/allenyou.nix
+++ user/allenyou.nix
@@ -1,17 +1,8 @@
-{ 
-    inputs,
-    lib,
-    config,
-    pkgs,
-    ...
-}:
+{ ... }:
 
 {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-        tree
-    ];
     openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFhiR8t5soqwfpcemRLTe9StCSGsD36TyJgE9PGcjJk cfan7@Allenyou-Laptop"
     ];
