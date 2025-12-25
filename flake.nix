7a90615fd81fc35e4d0a99766a8051e6f7fa8ae6
Allenyou <i@allenyou.wang>
2025-12-25 15:46:20 +0800

    尝试模块化

--- flake.nix
+++ flake.nix
@@ -7,40 +7,40 @@
 			url = "git+https://gitee.com/Allenyou1126/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-	};
-
-	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-		nixosConfigurations = {
-			sha-ali-gaal = nixpkgs.lib.nixosSystem {
-				specialArgs = {inherit inputs;};
-				modules = [
-					./ssh.nix
-					./nix-store-mirror.nix
-					./boot/efi-systemd.nix
-					./hardware-configuration.nix
-					./common.nix
-					./nix-store-mirror.nix
-					home-manager.nixosModules.home-manager {
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						home-manager.users.allenyou = import ./home/allenyou.nix;
-						home-manager.extraSpecialArgs = inputs;
-					}
-					({pkgs, ...}: {
-						environment.systemPackages = with pkgs; [
-							vim
-							wget
-							git
-						];
-						environment.variables.EDITOR = "vim";
-
-						system.stateVersion = "25.05";
-						networking.hostName = "sha-ali-gaal";
-						users.mutableUsers = false;
-						users.users.allenyou = import ./user/allenyou.nix;
-					})
-				];
-			};
+		haumea = {
+			url = "github:nix-community/haumea/v0.2.2";
+			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
+
+	outputs = let
+		users = import ./user { inherit inputs; };
+	in
+		{ self, nixpkgs, home-manager, ... }@inputs: {
+			nixosConfigurations = {
+				sha-ali-gaal = nixpkgs.lib.nixosSystem {
+					specialArgs = {inherit inputs;};
+					modules = [
+						./ssh.nix
+						./nix-store-mirror.nix
+						./boot/efi-systemd.nix
+						./hardware-configuration.nix
+						./common.nix
+						./nix-store-mirror.nix
+						users.userHomes
+						({pkgs, ...}: {
+							environment.systemPackages = with pkgs; [
+								vim
+								wget
+								git
+							];
+							environment.variables.EDITOR = "vim";
+							users = users.userCommons;
+							system.stateVersion = "25.05";
+							networking.hostName = "sha-ali-gaal";
+						})
+					];
+				};
+			};
+		};
 }
---
+++ user/default.nix
@@ -0,0 +1,35 @@
+{
+    inputs,
+    ...
+}:
+let
+    haumea = inputs.haumea;
+    home-manager = inputs.home-manager;
+    rawUsers = haumea.lib.load {
+        src = ./src;
+        inputs = { inherit inputs; };
+    };
+    userMapFunc = name: value: { userName = name; userSettings = value; };
+    loadedUsers = builtins.attrValues (builtins.mapAttrs userMapFunc rawUsers);
+    users = {
+        userHomes = home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = inputs;
+            home-manager.users = builtins.listToAttrs (map ({userName, userSettings}@user: {
+                name = userName;
+                value = {
+                    home.username = userName;
+                    home.homeDirectory = "/home/${userName}";
+                } // { inherit userSettings.home; };
+            }) loadedUsers);
+        };
+        userCommons = {
+            mutableUsers = false;
+            users = builtins.listToAttrs (map ({userName, userSettings}@user: {
+                name = userName;
+                value = { inherit userSettings.common; };
+            }) loadedUsers);
+        };
+    };
+in users
