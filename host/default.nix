d19f475f6499df62324ccd1bf83db4de62e51607
Allenyou <i@allenyou.wang>
2026-01-04 16:29:26 +0800

    修复格式

--- host/default.nix
+++ host/default.nix
@@ -17,29 +17,29 @@ let
     users = import ../user { inherit inputs pkgs; };
 
     hosts = builtins.listToAttrs (map ({hostName, hostSettings}@host: {
-            name = hostName;
-            value = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs; };
-                system = system;
-                modules = [
-                    inputs.agenix.nixosModules.default
-                    inputs.home-manager.nixosModules.home-manager
-                    {
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            extraSpecialArgs = { inherit inputs; };
-                            users = users.userHomes;
-                        };
-                    }
-                    ({ ... }: {
-                        users = users.userCommons;
-                        networking.hostName = hostName;
-                        system.stateVersion = "25.05";
-                    })
-                    hostSettings.common
-                ];
-            };
-        }) loadedHosts);
+        name = hostName;
+        value = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            system = system;
+            modules = [
+                inputs.agenix.nixosModules.default
+                inputs.home-manager.nixosModules.home-manager
+                {
+                    home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        extraSpecialArgs = { inherit inputs; };
+                        users = users.userHomes;
+                    };
+                }
+                ({ ... }: {
+                    users = users.userCommons;
+                    networking.hostName = hostName;
+                    system.stateVersion = "25.05";
+                })
+                hostSettings.common
+            ];
+        };
+    }) loadedHosts);
 
 in hosts
