f6e1c0df38fd9b8066d2e5dc5d4042881a64dc90
Allen You <i@allenyou.wang>
2026-01-10 01:21:22 +0800

    启用motd

--- modules/common.nix
+++ modules/common.nix
@@ -1,10 +1,14 @@
 { ... }:
 
 {
-    imports = [
-        ./motd.nix
-    ];
-    time.timeZone = "Asia/Shanghai";
-    nix.settings.experimental-features = ["nix-command" "flakes"];
-    i18n.defaultLocale = "en_US.UTF-8";
+  imports = [
+    ./motd.nix
+  ];
+  system.allenyou.motd.enable = true;
+  time.timeZone = "Asia/Shanghai";
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  i18n.defaultLocale = "en_US.UTF-8";
 }
