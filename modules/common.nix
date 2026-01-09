029ba2a2dd10d32fd9a774aa5e77f4cb0d402448
Allen You <i@allenyou.wang>
2026-01-10 00:54:47 +0800

    添加motd

--- modules/common.nix
+++ modules/common.nix
@@ -1,6 +1,9 @@
 { ... }:
 
 {
+    imports = [
+        ./motd.nix
+    ];
     time.timeZone = "Asia/Shanghai";
     nix.settings.experimental-features = ["nix-command" "flakes"];
     i18n.defaultLocale = "en_US.UTF-8";
