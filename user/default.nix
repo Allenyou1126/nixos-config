63c7f293ce79506809e32f67594c034dbfd635b5
Allen You <i@allenyou.wang>
2026-01-10 01:14:33 +0800

    修复

--- user/default.nix
+++ user/default.nix
@@ -1,45 +1,69 @@
 {
-    inputs,
-    pkgs,
-    ...
+  inputs,
+  pkgs,
+  ...
 }:
 let
-    inherit (inputs) nixpkgs haumea;
-    lib = nixpkgs.lib;
-    loadToAllUsers = haumea.lib.load {
-        src = ./load-to-all;
-        inputs = { inherit inputs pkgs; };
-    };
-    userMapFunc = name: value: { userName = name; userSettings = value; };
-    loadedUsers = builtins.attrValues (builtins.mapAttrs userMapFunc loadToAllUsers );
-    users = {
-        userHomes = builtins.listToAttrs (map ({userName, userSettings}@user: {
+  inherit (inputs) nixpkgs haumea;
+  lib = nixpkgs.lib;
+  loadToAllUsers = haumea.lib.load {
+    src = ./load-to-all;
+    inputs = { inherit inputs pkgs; };
+  };
+  userMapFunc = name: value: {
+    userName = name;
+    userSettings = value;
+  };
+  loadedUsers = builtins.attrValues (builtins.mapAttrs userMapFunc loadToAllUsers);
+  users = {
+    userHomes = builtins.listToAttrs (
+      map (
+        { userName, userSettings }:
+        {
+          name = userName;
+          value = lib.mkMerge {
+            home.username = userName;
+            home.homeDirectory = "/home/${userName}";
+          } userSettings.home;
+        }
+      ) loadedUsers
+    );
+    userHomesClient = builtins.listToAttrs (
+      map (
+        { userName, userSettings }:
+        {
+          name = userName;
+          value = lib.mkMerge {
+            home.username = userName;
+            home.homeDirectory = "/home/${userName}";
+          } (lib.mkMerge userSettings.home (userSettings.home-client or { }));
+        }
+      ) loadedUsers
+    );
+    userCommons = {
+      mutableUsers = false;
+      users = builtins.listToAttrs (
+        map (
+          { userName, userSettings }:
+          {
             name = userName;
-            value = lib.recursiveUpdate {
-                home.username = userName;
-                home.homeDirectory = "/home/${userName}";
-            } userSettings.home;
-        }) loadedUsers);
-        userHomesClient = builtins.listToAttrs (map ({userName, userSettings}@user: {
-            name = userName;
-            value = lib.recursiveUpdate {
-                home.username = userName;
-                home.homeDirectory = "/home/${userName}";
-            } (lib.recursiveUpdate userSettings.home (userSettings.home-client or {}));
-        }) loadedUsers);
-        userCommons = {
-            mutableUsers = false;
-            users = builtins.listToAttrs (map ({userName, userSettings}@user: {
-                name = userName;
-                value = userSettings.common;
-            }) loadedUsers);
-        };
-        userCommonsClient = {
-            mutableUsers = false;
-            users = builtins.listToAttrs (map ({userName, userSettings}@user: {
-                name = userName;
-                value = lib.recursiveUpdate userSettings.common (userSettings.common-client or {});
-            }) loadedUsers);
-        };
+            value = userSettings.common;
+          }
+        ) loadedUsers
+      );
     };
-in users
+    userCommonsClient = {
+      mutableUsers = false;
+      users = builtins.listToAttrs (
+        map (
+          { userName, userSettings }:
+          {
+            name = userName;
+            value = lib.mkMerge userSettings.common (userSettings.common-client or { });
+          }
+        ) loadedUsers
+      );
+    };
+  };
+in
+users
