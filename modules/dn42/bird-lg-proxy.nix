b191c204cada4bd5b0c45b18ed15ca9919866cf3
Allen You <i@allenyou.wang>
2026-01-16 08:19:25 +0800

    格式化代码

--- modules/dn42/bird-lg-proxy.nix
+++ modules/dn42/bird-lg-proxy.nix
@@ -1,59 +1,70 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
-    cfg = config.services.dn42.bird-lg-proxy;
-    bird-lg-proxy-go-pkg = pkgs.callPackage ../../pkgs/bird-lg-proxy-go.nix { };
-in {
-    options.services.dn42.bird-lg-proxy = {
-        enable = lib.mkEnableOption "Bird-lg-proxy for DN42 networking (requires bird to be enabled)";
-        port = lib.mkOption {
-            type = lib.types.port;
-            default = 8000;
-            description = "Port to listen on";
-        };
-        address = lib.mkOption {
-            type = lib.types.str;
-            default = "0.0.0.0";
-            description = "Address to listen on";
-        };
+  cfg = config.services.dn42.bird-lg-proxy;
+  bird-lg-proxy-go-pkg = pkgs.callPackage ../../pkgs/bird-lg-proxy-go.nix { };
+in
+{
+  options.services.dn42.bird-lg-proxy = {
+    enable = lib.mkEnableOption "Bird-lg-proxy for DN42 networking (requires bird to be enabled)";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = bird-lg-proxy-go-pkg;
+      description = "Package to use for bird-lg-proxy";
     };
-
-    config = lib.mkIf (cfg.enable && config.services.dn42.bird2.enable) {
-        systemd.services.bird-lg-proxy = {
-            enable = true;
-            description = "Bird-lg-proxy";
-            wantedBy = [ "multi-user.target" ];
-            path = with pkgs; [
-                traceroute
-            ];
-            environment = {
-                BIRD_SOCKET = "/run/bird/bird.ctl";
-                BIRDLG_LISTEN = "${cfg.address}:${toString cfg.port}";
-            };
-            unitConfig = {
-                After = "bird.service";
-            };
-            serviceConfig = {
-                Type = "simple";
-                Restart = "always";
-                RestartSec = "3";
-                ExecStart = lib.getExe bird-lg-proxy-go-pkg;
-
-                # Needed by mtr and traceroute
-                AmbientCapabilities = [
-                    "CAP_NET_ADMIN"
-                    "CAP_NET_RAW"
-                ];
-                CapabilityBoundingSet = [
-                    "CAP_NET_ADMIN"
-                    "CAP_NET_RAW"
-                ];
-                SystemCallFilter = [ ];
-
-                Group = "bird";
-                User = "bird";
-            };
-        };
-	networking.firewall.allowedTCPPorts = [ cfg.port ];
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 8000;
+      description = "Port to listen on";
     };
+    address = lib.mkOption {
+      type = lib.types.str;
+      default = "0.0.0.0";
+      description = "Address to listen on";
+    };
+  };
+
+  config = lib.mkIf (cfg.enable && config.services.dn42.bird2.enable) {
+    systemd.services.bird-lg-proxy = {
+      enable = true;
+      description = "Bird-lg-proxy";
+      wantedBy = [ "multi-user.target" ];
+      path = with pkgs; [
+        traceroute
+      ];
+      environment = {
+        BIRD_SOCKET = "/run/bird/bird.ctl";
+        BIRDLG_LISTEN = "${cfg.address}:${toString cfg.port}";
+      };
+      unitConfig = {
+        After = "bird.service";
+      };
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "3";
+        ExecStart = lib.getExe bird-lg-proxy-go-pkg;
+
+        # Needed by mtr and traceroute
+        AmbientCapabilities = [
+          "CAP_NET_ADMIN"
+          "CAP_NET_RAW"
+        ];
+        CapabilityBoundingSet = [
+          "CAP_NET_ADMIN"
+          "CAP_NET_RAW"
+        ];
+        SystemCallFilter = [ ];
+
+        Group = "bird";
+        User = "bird";
+      };
+    };
+    networking.firewall.allowedTCPPorts = [ cfg.port ];
+  };
 }
--- pkgs/bird-lg-proxy-go.nix
+++ pkgs/bird-lg-proxy-go.nix
@@ -1,26 +1,25 @@
 {
-  buildGoModule,
   lib,
   pkgs,
 }:
 pkgs.buildGoModule (finalAttrs: {
-    pname = "bird-lgproxy-go";
-    version = "v1.4.0";
-    src = pkgs.fetchFromGitHub {
-        owner = "xddxdd";
-        repo = "bird-lg-go";
-        tag = "v1.4.0";
-        hash = "sha256-7VF0MFNa3edKOWmMBib0DxlB1c1h3qBieA4YeUAuBzM=";
-    };
-    vendorHash = "sha256-iosWHHeJyqMPF+Y01+mj70HDKWw0FAZKDpEESAwS/i4=";
+  pname = "bird-lgproxy-go";
+  version = "v1.4.0";
+  src = pkgs.fetchFromGitHub {
+    owner = "xddxdd";
+    repo = "bird-lg-go";
+    tag = "v1.4.0";
+    hash = "sha256-7VF0MFNa3edKOWmMBib0DxlB1c1h3qBieA4YeUAuBzM=";
+  };
+  vendorHash = "sha256-iosWHHeJyqMPF+Y01+mj70HDKWw0FAZKDpEESAwS/i4=";
 
-    modRoot = "proxy";
+  modRoot = "proxy";
 
-    meta = {
-        mainProgram = "proxy";
-        maintainers = with lib.maintainers; [ xddxdd ];
-        description = "BIRD looking glass in Go, for better maintainability, easier deployment & smaller memory footprint";
-        homepage = "https://github.com/xddxdd/bird-lg-go";
-        license = lib.licenses.gpl3Only;
-    };
+  meta = {
+    mainProgram = "proxy";
+    maintainers = with lib.maintainers; [ xddxdd ];
+    description = "BIRD looking glass in Go, for better maintainability, easier deployment & smaller memory footprint";
+    homepage = "https://github.com/xddxdd/bird-lg-go";
+    license = lib.licenses.gpl3Only;
+  };
 })
