2a013e0c127ea9cf17d15002717e1ddb896d3cb8
Allen You <i@allenyou.wang>
2026-01-23 09:54:18 +0800

    添加 nginx 配置

---
+++ host/src/sha-ali-seldon/nginx.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+  services.nginx = {
+    enable = true;
+    package = pkgs.nginxMainline;
+    recommendedBrotliSettings = true;
+    recommendedGzipSettings = true;
+    virtualHosts = {
+      default = {
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 80;
+            extraParameters = [
+              "default_server"
+            ];
+          }
+        ];
+        root = "/var/www/html";
+        serverName = "homepage-cdn-origin.allenyou.top";
+        locations = {
+          "/" = {
+            tryFiles = "$uri $uri/ =404";
+          };
+        };
+        extraConfig = ''
+          if ($http_aliyun_cdn_source_allenyou != 'true') {
+            return 444;
+          }
+        '';
+      };
+    };
+  };
+}
