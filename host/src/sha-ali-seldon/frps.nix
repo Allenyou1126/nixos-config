d3e13a88aae4cba204d8f571cf1ff55621acef46
Allen You <i@allenyou.wang>
2026-01-23 08:58:54 +0800

    添加sha-ali-seldon配置

---
+++ host/src/sha-ali-seldon/frps.nix
@@ -0,0 +1,52 @@
+{ config, ... }:
+
+let
+  listenPort = 20000;
+  allowedPortRange = {
+    start = 20001;
+    end = 20099;
+  };
+  changeToFirewall = portRange: {
+    from = portRange.start;
+    to = portRange.end;
+  };
+in
+{
+  services.frp = {
+    enable = true;
+    role = "server";
+    settings = {
+      bindAddr = "0.0.0.0";
+      bindPort = listenPort;
+      quicBindPort = listenPort;
+
+      transport.maxPoolCount = 2000;
+      transport.tcpMux = true;
+      transport.tcpMuxKeepaliveInterval = 60;
+      transport.tcpKeepalive = 7200;
+      transport.tls.force = false;
+
+      log.to = "/dev/stdout";
+      log.level = "info";
+      log.disablePrintColor = false;
+
+      auth.method = "token";
+      auth.tokenSource = {
+        type = "file";
+        file = {
+          path = config.age.secrets.frp-token.path;
+        };
+      };
+      allowPorts = [
+        allowedPortRange
+      ];
+      maxPortsPerClient = 8;
+      udpPacketSize = 1500;
+      natholeAnalysisDataReserveHours = 168;
+    };
+  };
+  networking.firewall.allowedUDPPortRanges = [ (changeToFirewall allowedPortRange) ];
+  networking.firewall.allowedTCPPortRanges = [ (changeToFirewall allowedPortRange) ];
+  networking.firewall.allowedTCPPorts = [ listenPort ];
+  networking.firewall.allowedUDPPorts = [ listenPort ];
+}
