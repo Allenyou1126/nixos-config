8b45e15743a5c28c5bd9e5229bea58d646386ffa
Allen You <i@allenyou.wang>
2026-01-24 16:00:26 +0800

    添加Binary Cache

---
+++ modules/cachix/nixpkgs.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://nixpkgs.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+      ];
+    };
+  };
+}
