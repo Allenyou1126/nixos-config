80125e19f287272b98f8fbce4a9524e39786f971
Allen You <i@allenyou.wang>
2026-01-26 17:55:26 +0800

    添加Cache key

--- modules/binary-cache/allenyou-oss-cache.nix
+++ modules/binary-cache/allenyou-oss-cache.nix
@@ -6,6 +6,7 @@
       ];
       trusted-public-keys = [
         "allenyou-nix-cache.lap-fallom.1:rKfa65SLrDQLTGEmp+c90R4vsnGgD9xFoKHrdrvJGRs="
+        "allenyou-nix-cache.lax-rn-riose.1:zGDPA3NkInSSjfH5hrYBwYAIu0JyCyE9LDKl/mcDFWE="
       ];
     };
   };
