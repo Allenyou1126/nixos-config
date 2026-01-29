6944b8919f4b68c6e3e2dd77ecaa15592270fa95
Allen You <i@allenyou.wang>
2026-01-29 10:52:07 +0800

    修复

--- host/src/sha-ali-seldon/secrets.nix
+++ host/src/sha-ali-seldon/secrets.nix
@@ -8,7 +8,6 @@ allenyou-secrets:
   frp-token = {
     file = "${allenyou-secrets}/frp-token.sha-ali-seldon.age";
     path = "/etc/frp/token";
-    mode = "755";
-    owner = "frp";
+    mode = "111";
   };
 }
