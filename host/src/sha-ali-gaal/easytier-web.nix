5e8861f1f715c9461baed0b09d8b0157b4255e25
Allen You <i@allenyou.wang>
2026-02-04 09:57:42 +0800

    部署Easytier Web

---
+++ host/src/sha-ali-gaal/easytier-web.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  services.easytier-web = {
+    enable = true;
+    port = 22021;
+    configPort = 22020;
+    openFirewall = false;
+    configProtocol = "http";
+  };
+  networking.firewall.allowedTCPPorts = [ 22020 ];
+  networking.firewall.extraConfig = ''
+    iptables -A INPUT -s 172.24.24.198 -p tcp -m tcp --dport 22021 -j ACCEPT
+  '';
+}
---
+++ modules/easytier-web.nix
@@ -0,0 +1,70 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.services.easytier-web;
+in
+{
+  options.services.easytier-web = {
+    enable = lib.mkEnableOption "Web API for Easytier networking";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.easytier;
+      description = "Package to use for easytier-web";
+    };
+    apiPort = lib.mkOption {
+      type = lib.types.port;
+      default = 11211;
+      description = "Port to listen on";
+    };
+    configPort = lib.mkOption {
+      type = lib.types.port;
+      default = 22020;
+      description = "Port to listen on for config requests";
+    };
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Open the firewall for the API port";
+    };
+    configProtocol = lib.mkOption {
+      type = lib.types.enum [
+        "tcp"
+        "udp"
+        "ws"
+      ];
+      default = "udp";
+      description = "Protocol to use for distribution of config";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.easytier-web = {
+      enable = true;
+      description = "Easytier Web API";
+      wantedBy = [ "multi-user.target" ];
+      unitConfig = {
+        After = "bird.service";
+      };
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "3";
+        ExecStart = "${cfg.package}/bin/easytier-web --api-server-port ${toString cfg.apiPort} --config-server-port ${toString cfg.configPort} --config-server-protocol ${cfg.configProtocol}";
+
+        SystemCallFilter = [ ];
+
+        Group = "easytier";
+        User = "easytier";
+      };
+    };
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [
+      cfg.apiPort
+      cfg.configPort
+    ];
+  };
+}
