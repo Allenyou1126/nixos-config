7babf70ad099662d7b4d5f83f2bc37a1b57601d3
Allen You <i@allenyou.wang>
2026-03-14 01:32:25 +0800

    修复

--- host/src/hkg-dog-darell/certimate.nix
+++ host/src/hkg-dog-darell/certimate.nix
@@ -4,6 +4,5 @@
   systemd.services.certimate.serviceConfig = lib.mkForce {
     ExecStart = "${pkgs.allenyou-nur.certimate}/bin/certimate serve --dir /var/certimate/ --http 127.0.0.1:8090";
     Restart = lib.mkForce "always";
-    User = "certimate";
   };
 }
