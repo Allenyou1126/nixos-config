8461df00b57ff7669f0b3c25a8413d04a11b906c
Allen You <i@allenyou.wang>
2026-03-13 20:55:59 +0800

    添加hkg-dog-darell

--- host/secrets.nix
+++ host/secrets.nix
@@ -7,4 +7,10 @@ allenyou-secrets:
     owner = "allenyou";
     mode = "0600";
   };
+  aliyun-cli-config = {
+    file = "${allenyou-secrets}/aliyun-cli-config.age";
+    path = "/home/allenyou/.aliyun/config.json";
+    owner = "allenyou";
+    mode = "0600";
+  };
 }
