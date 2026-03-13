a68cbc6319111324226e1b2ca7f5d4a0aaf45a5c
Allen You <i@allenyou.wang>
2026-03-13 20:58:17 +0800

    启用Nginx

---
+++ host/src/hkg-dog-darell/nginx.nix
@@ -0,0 +1,73 @@
+{ pkgs, ... }:
+
+let
+  nginxPackage = pkgs.nginxMainline;
+in
+
+{
+  services.nginx = {
+    enable = true;
+    package = nginxPackage;
+    recommendedBrotliSettings = true;
+    recommendedGzipSettings = true;
+    statusPage = true;
+    virtualHosts = {
+      default = {
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 80;
+            extraParameters = [
+              "default_server"
+            ];
+          }
+          {
+            addr = "0.0.0.0";
+            port = 443;
+            extraParameters = [
+              "default_server"
+            ];
+          }
+        ];
+        reuseport = true;
+        locations = {
+          "/" = {
+            return = 444;
+          };
+        };
+        default = true;
+      };
+    };
+  };
+  security.sudo.extraRules = [
+    {
+      users = [
+        "certimate"
+      ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/systemctl reload nginx";
+          options = [
+            "NOPASSWD"
+          ];
+        }
+      ];
+    }
+  ];
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+  networking.firewall.allowedUDPPorts = [
+    80
+    443
+  ];
+  services.prometheus.exporters.nginx = {
+    enable = true;
+    listenAddress = "0.0.0.0";
+    port = 9090;
+  };
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -s 139.196.157.228 -p tcp -m tcp --dport 9090 -j ACCEPT
+  '';
+}
