b6b542c89b987a110bb641c6b2d6e27eccf6a94a
Allen You <i@allenyou.wang>
2026-03-14 11:27:25 +0800

    修复

--- host/src/hkg-dog-darell/secrets.nix
+++ host/src/hkg-dog-darell/secrets.nix
@@ -16,7 +16,13 @@ allenyou-secrets:
   # wakapi-database-password.file = "${allenyou-secrets}/wakapi-database-password.hkg-dog-darell.age";
   # waline-secrets.file = "${allenyou-secrets}/waline-secrets.hkg-dog-darell.age";
   # waline-zlight106-secrets.file = "${allenyou-secrets}/waline-zlight106-secrets.hkg-dog-darell.age";
-  umami-app-secret.file = "${allenyou-secrets}/umami-app-secret.hkg-dog-darell.age";
-  umami-database-url.file = "${allenyou-secrets}/umami-database-url.hkg-dog-darell.age";
+  umami-app-secret = {
+    file = "${allenyou-secrets}/umami-app-secret.hkg-dog-darell.age";
+    mode = "0444";
+  };
+  umami-database-url = {
+    file = "${allenyou-secrets}/umami-database-url.hkg-dog-darell.age";
+    mode = "0444";
+  };
   webdav-password.file = "${allenyou-secrets}/webdav-password.hkg-dog-darell.age";
 }
