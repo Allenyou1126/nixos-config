e9e620d46e84fd1a1ccb0c0878e95804889b4e2b
Allen You <i@allenyou.wang>
2026-04-08 17:04:12 +0800

    更新Flake Input

--- modules/motd.nix
+++ modules/motd.nix
@@ -21,50 +21,50 @@ let
         printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Role" "${cfg.description}"
       '';
   motd = pkgs.writeShellScriptBin "motd" ''
-    		#! /usr/bin/env bash
-    		source /etc/os-release
-    		RED="\e[31m"
-    		GREEN="\e[32m"
-    		BOLD="\e[1m"
-    		ENDCOLOR="\e[0m"
-    		LOAD1=`cat /proc/loadavg | awk {'print $1'}`
-    		LOAD5=`cat /proc/loadavg | awk {'print $2'}`
-    		LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+    #! /usr/bin/env bash
+    source /etc/os-release
+    RED="\e[31m"
+    GREEN="\e[32m"
+    BOLD="\e[1m"
+    ENDCOLOR="\e[0m"
+    LOAD1=`cat /proc/loadavg | awk {'print $1'}`
+    LOAD5=`cat /proc/loadavg | awk {'print $2'}`
+    LOAD15=`cat /proc/loadavg | awk {'print $3'}`
 
-    		MEMORY=`free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100 / $2 }'`
+    MEMORY=`free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100 / $2 }'`
 
-    		# time of day
-    		HOUR=$(date +"%H")
-    		if [ $HOUR -lt 12  -a $HOUR -ge 0 ]
-    		then TIME="morning"
-    		elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]
-    		then TIME="afternoon"
-    		else
-    			TIME="evening"
-    		fi
+    # time of day
+    HOUR=$(date +"%H")
+    if [ $HOUR -lt 12  -a $HOUR -ge 0 ]
+    then TIME="morning"
+    elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]
+    then TIME="afternoon"
+    else
+      TIME="evening"
+    fi
 
 
-    		uptime=`cat /proc/uptime | cut -f1 -d.`
-    		upDays=$((uptime/60/60/24))
-    		upHours=$((uptime/60/60%24))
-    		upMins=$((uptime/60%60))
-    		upSecs=$((uptime%60))
+    uptime=`cat /proc/uptime | cut -f1 -d.`
+    upDays=$((uptime/60/60/24))
+    upHours=$((uptime/60/60%24))
+    upMins=$((uptime/60%60))
+    upSecs=$((uptime%60))
 
-    		${displayBanner}
+    ${displayBanner}
 
-    		printf "$BOLD Welcome to $(hostname)!$ENDCOLOR\n"
-    		${displayDescription}
-    		printf "\n"
-    		printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Release" "$PRETTY_NAME"
-    		printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Kernel" "$(uname -rs)"
-    		[ -f /var/run/reboot-required ] && printf "\n$RED  * %-20s$ENDCOLOR %s\n" "A reboot is required"
-    		printf "\n"
-    		printf "$BOLD  * %-20s$ENDCOLOR %s\n" "CPU usage" "$LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)"
-    		printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Memory" "$MEMORY"
-    		printf "$BOLD  * %-20s$ENDCOLOR %s\n" "System uptime" "$upDays days $upHours hours $upMins minutes $upSecs seconds"
+    printf "$BOLD Welcome to $(hostname)!$ENDCOLOR\n"
+    ${displayDescription}
+    printf "\n"
+    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Release" "$PRETTY_NAME"
+    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Kernel" "$(uname -rs)"
+    [ -f /var/run/reboot-required ] && printf "\n$RED  * %-20s$ENDCOLOR %s\n" "A reboot is required"
+    printf "\n"
+    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "CPU usage" "$LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)"
+    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Memory" "$MEMORY"
+    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "System uptime" "$upDays days $upHours hours $upMins minutes $upSecs seconds"
 
-    		printf "\n"
-    	'';
+    printf "\n"
+  '';
 in
 {
   options.system.allenyou.motd = {
