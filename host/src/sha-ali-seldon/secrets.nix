8e0c69b7bc67115fdc26a87eea80e5f4e0db3526
Allen You <i@allenyou.wang>
2026-04-29 10:39:48 +0800

    修复

--- host/src/sha-ali-seldon/secrets.nix
+++ host/src/sha-ali-seldon/secrets.nix
@@ -5,6 +5,7 @@ allenyou-secrets:
   wireguardPrivateKeyFile.file = "${allenyou-secrets}/wg-private-key.sha-ali-seldon.age";
   lap-fallom-wireguardPresharedKeyFile.file = "${allenyou-secrets}/wg-psk-lap-fallom.sha-ali-seldon.age";
   home-hardin-wireguardPresharedKeyFile.file = "${allenyou-secrets}/wg-psk-home-hardin.sha-ali-seldon.age";
+  lap-yenner-wireguardPresharedKeyFile.file = "${allenyou-secrets}/wg-psk-lap-yenner.sha-ali-seldon.age";
   frp-token = {
     file = "${allenyou-secrets}/frp-token.sha-ali-seldon.age";
     path = "/etc/frp/token";
