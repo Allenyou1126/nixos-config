61273c109c98c3e1355db231a8cb1c79f1b9bef5
Allenyou <i@allenyou.wang>
2026-09-23 22:55:43 +0800

    更新HermesAgent模型

--- host/src/hkg-dog-darell/apps/hermes/model.nix
+++ host/src/hkg-dog-darell/apps/hermes/model.nix
@@ -7,10 +7,10 @@
         name = "xcpc-ai";
         base_url = "https://xcpcai.com/v1";
         models = {
-          "gpt-5.6-sol" = {
+          "gpt-6-sol" = {
             context_length = 400000;
           };
-          "gpt-5.6-luna" = {
+          "gpt-6-luna" = {
             context_length = 400000;
           };
         };
@@ -19,45 +19,45 @@
       }
     ];
     agent = {
-      reasoning_effort = "max";
+      reasoning_effort = "high";
     };
     model = {
       provider = "xcpc-ai";
-      default = "gpt-5.6-sol";
+      default = "gpt-6-sol";
     };
     auxiliary = {
       vision = {
         provider = "xcpc-ai";
-        model = "gpt-5.6-luna";
+        model = "gpt-6-luna";
       };
       web_extract = {
         provider = "xcpc-ai";
-        model = "gpt-5.6-luna";
+        model = "gpt-6-luna";
       };
       approval = {
         provider = "xcpc-ai";
-        model = "gpt-5.6-luna";
+        model = "gpt-6-luna";
       };
       session_search = {
         provider = "xcpc-ai";
-        model = "gpt-5.6-luna";
+        model = "gpt-6-luna";
       };
       skills_hub = {
         provider = "xcpc-ai";
-        model = "gpt-5.6-luna";
+        model = "gpt-6-luna";
       };
       mcp = {
         provider = "xcpc-ai";
-        model = "gpt-5.6-luna";
+        model = "gpt-6-luna";
       };
       flush_memories = {
         provider = "xcpc-ai";
-        model = "gpt-5.6-luna";
+        model = "gpt-6-luna";
       };
     };
     compression = {
       enabled = true;
-      summary_model = "gpt-5.6-luna";
+      summary_model = "gpt-6-luna";
       summary_provider = "main";
     };
   };
